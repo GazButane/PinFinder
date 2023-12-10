b45eb002ed5e0f33120c535182410f7447af5377
Gaz_Butane <gaz.butane.pr@gmail.com>
2023-12-10 14:45:26 +0100

    calc time improved

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ print(" ______  _____  ______   ______ _____  ______   _____    ______  ______  
 print("| |  | \  | |  | |  \ \ | |      | |  | |  \ \ | | \ \  | |     | |  | \ ")
 print("| |__|_/  | |  | |  | | | |----  | |  | |  | | | |  | | | |---- | |__| | ")
 print("|_|      _|_|_ |_|  |_| |_|     _|_|_ |_|  |_| |_|_/_/  |_|____ |_|  \_\ ")
+print("Test the strength of your pin code...")
+print(" ")
 print("°°°°°°°°°°°°°°°°°")
-print("°  Patametres   °")
+print("°   Settings    °")
 print("°°°°°°°°°°°°°°°°°")
-codesecret = str(input("Entrez code: "))
-caracteres = int(input("Nombre de digits: "))
+codesecret = str(input("Enter a PIN: "))
+caracteres = int(input("Number of digits: "))
 grandeur = 10**caracteres-1
+tinygrandeur = 10**(caracteres-1)-1
 startTime = time.perf_counter()
 
 print("°°°°°°°°°°°°°°°°°")
@@ -21,17 +24,20 @@ print("°    Running    °")
 print("°°°°°°°°°°°°°°°°°")
 
 while trouvé == False:
-    test = str(random.randint(0,grandeur))
+    test = str(random.randint(tinygrandeur,grandeur))
     sys.stdout.write(f"\r{test}")
     tentatives += 1
 
     if test == codesecret:
         endTime = time.perf_counter()
         diff = endTime - startTime
-        print("  <- Trouvé !")
+        print("  <- Found !")
         print("°°°°°°°°°°°°°°°°°")
         print("°    Finish!    °")
         print("°°°°°°°°°°°°°°°°°")
-        print(f"Trouvé en {tentatives} essais")
-        print(f"Temps de calcul: {diff} s")
+        print(f"Found in {tentatives} tries")
+        if diff < 60:
+            print(f"Calculation time: {diff} s")
+        else:
+            print(f"Calculation time: {diff/60} min")
         trouvé = True
